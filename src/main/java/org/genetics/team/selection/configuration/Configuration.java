051bc8de8de3b77dd406a732aa65616a064a5766
Madawa Soysa <madawa.rc@gmail.com>
2017-03-19 20:35:38 +0530

    ConfigurationManager initial

---
+++ src/main/java/org/genetics/team/selection/configuration/Configuration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2017 Madawa Soysa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.genetics.team.selection.configuration;
+
+import java.util.List;
+
+/**
+ * This class holds the configuration
+ */
+public final class Configuration {
+
+    private String populationData;
+    private List<String> excluded;
+    private Double mutationRate;
+
+    public String getPopulationData() {
+        return populationData;
+    }
+
+    public void setPopulationData(String populationData) {
+        this.populationData = populationData;
+    }
+
+    public List<String> getExcluded() {
+        return excluded;
+    }
+
+    public void setExcluded(List<String> excluded) {
+        this.excluded = excluded;
+    }
+
+    public Double getMutationRate() {
+        return mutationRate;
+    }
+
+    public void setMutationRate(Double mutationRate) {
+        this.mutationRate = mutationRate;
+    }
+}
