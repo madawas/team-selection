2fc094b1d280c381a6a936ddb5e77ee5ea33743d
Madawa Soysa <madawa.rc@gmail.com>
2017-05-11 16:09:42 +0530

    Removing LH

--- src/main/java/org/genetics/team/selection/configuration/ConfigurationManager.java
+++ src/main/java/org/genetics/team/selection/configuration/ConfigurationManager.java
@@ -1,24 +1,9 @@
-/*
- * Copyright 2017 Madawa Soysa
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.genetics.team.selection.configuration;
 
 import org.genetics.team.selection.util.CommonConstants;
 import org.yaml.snakeyaml.Yaml;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -35,6 +20,11 @@ public class ConfigurationManager {
         Yaml yaml = new Yaml();
         try (InputStream in = Files.newInputStream(Paths.get(configPath))) {
             configuration = yaml.loadAs(in, Configuration.class);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(new JFrame(),
+                    "Error occurred when reading the input config.", "Dialog",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(-1);
         }
 
         if(configuration.getPopulationData() == null) {
--- src/main/java/org/genetics/team/selection/gui/GUIForm.java
+++ src/main/java/org/genetics/team/selection/gui/GUIForm.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Madawa Soysa
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.genetics.team.selection.gui;
 
 import com.intellij.uiDesigner.core.GridConstraints;
@@ -351,6 +335,10 @@ public class GUIForm {
                     mainForm.inputProcessor.readPopulation(mainForm.appConfiguration.getPopulationData()));
         } catch (IOException e) {
             log.error("Error occurred when reading the input file.", e);
+            JOptionPane.showMessageDialog(new JFrame(),
+                    "Error occurred when reading the input file.", "Dialog",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(-1);
         }
         mainForm.frame.setVisible(true);
     }
